c69956bf2283ff13dcdf4bc5e5c115dbcdc280d7
yiapou13 <yiapou13@users.noreply.github.com>
2019-12-13 00:31:17 +0000

    Deploy

---
+++ precache-manifest.1da436a75de5e989afee8c8448c35265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d353eaa4d03a2646d505",
+    "url": "css/app.1c82a2fd.css"
+  },
+  {
+    "revision": "d353eaa4d03a2646d505",
+    "url": "js/app.7639acd2.js"
+  },
+  {
+    "revision": "b980a33a9039d859030b",
+    "url": "js/chunk-vendors.d4a7c842.js"
+  },
+  {
+    "revision": "6755a261d60a112d6c7233278ea3942f",
+    "url": "index.html"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  }
+];
